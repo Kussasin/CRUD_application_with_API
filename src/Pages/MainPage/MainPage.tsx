d867f2f73bd1668ba570ee564c88a2bcbce6e4b8
Bogdan Basisty <bohdan.basistyi@gmail.com>
2023-05-01 21:10:48 +0200

    FE #5 - Add Axios

--- src/Pages/MainPage/MainPage.tsx
+++ src/Pages/MainPage/MainPage.tsx
@@ -3,6 +3,7 @@ import { increment, decrement } from '../../Store/CounterStore';
 import { RootState } from '../../Store/CounterStore';
 import styles from './MainPage.module.scss';
 import ModalWindow from '../../Components/ModalWindow/ModalWindow';
+import api from '../../Api/Instance';
 
 interface ModalContent {
     header: string;
@@ -13,6 +14,13 @@ const companyList: ModalContent = {
     header: "I'm a modal dialog",
     content: "Modal content goes here..."
 }
+
+api.checkStatus().then((response) => {
+    const { status_code, detail, result } = response.data;
+    console.log(`status_code: ${status_code}, detail: ${detail}, result: ${result}`);
+}).catch((error) => {
+    console.error(`Ошибка при получении состояния бекенда: ${error.message}`);
+});
 
 const MainPage = () => {
     const count = useSelector((state: RootState) => state.counter);
